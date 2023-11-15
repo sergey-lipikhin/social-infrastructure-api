5eb0d55e582433c9ae9178487eaa1e6034715f5b
Sergey Lipikhin <football3@ukr.net>
2023-11-15 11:09:09 +0200

    Make db be remote for Stasik

--- apps/server/src/utils/initDb.ts
+++ apps/server/src/utils/initDb.ts
@@ -7,15 +7,24 @@ import { User } from '../models/User';
 import { Config } from '../models/Config';
 
 export const initDb = async(): Promise<Sequelize> => {
-  const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: false,
-    models: [User, Rating, Experiment, Config],
-  });
+  // const sequelize = new Sequelize({
+  //   dialect: 'postgres',
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   logging: false,
+  //   models: [User, Rating, Experiment, Config],
+  // });
+
+  const sequelize = new Sequelize(
+    'postgresql://jabroni%40ukr.net:P9Nt1yvLnDmJ@ep-wild-art-65945557.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    {
+      dialect: 'postgres',
+      logging: false,
+      models: [User, Rating, Experiment, Config],
+    },
+  );
 
   try {
     sequelize.authenticate({ logging: false });
